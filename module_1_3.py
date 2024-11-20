34aafbcfdb2e2c3b207301370527ffdc634de23c
elizaveta <karpemkoliza@icloud.com>
2024-11-20 19:50:13 +1000

    20/11

---
+++ module_1_3.py
@@ -0,0 +1,23 @@
+name = 'Лиза'
+print(name)
+age = 24
+print(age)
+age += 34
+print(age)
+is_Student = True
+print(is_Student)
+
+
+homework = 12
+watch_count = 1.5
+course_name = 'python'
+time_one = watch_count / homework
+
+print(homework, watch_count, course_name, time_one)
+
+example = 'Я хочу больше уроков'
+print(example[0])
+print(example[-1])
+print(example[10:])
+print(example[::-1])
+print(example[1::2])
