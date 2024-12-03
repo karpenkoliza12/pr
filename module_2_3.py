131824973ddbe9210b78e3de685f340f34832a66
elizaveta <karpemkoliza@icloud.com>
2024-12-03 14:29:55 +1000

    3/12

--- module_2_3.py
+++ module_2_3.py
@@ -1,6 +1,6 @@
-my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+my_list = [42, 69, 322, 13, 0, 99]
 i = 0
-while i <= len(my_list):
+while i < len(my_list):
     if my_list[i] > 0:
         print(my_list[i])
     elif my_list[i] < 0:
