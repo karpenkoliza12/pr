6e65fd3b8b83ae2fb1cfa7318d968b62e07db277
elizaveta <karpemkoliza@icloud.com>
2024-12-12 21:58:51 +1000

    12/12

--- module_3_1.py
+++ module_3_1.py
@@ -1,16 +1,20 @@
 calls = 0
+
+
 def count_calls():
     global calls
-    calls +=1
+    calls += 1
+
 
 def string_info(string):
     count_calls()
-    return (len(string), string.upper(), string.lower())
+    return len(string), string.upper(), string.lower()
+
+
 def is_contains(string, list_to_search):
     count_calls()
     string = string.lower()
     for i in list_to_search:
-        string_to_search = True
         if i.lower() == string:
             string_to_search = True
             return string_to_search
@@ -18,8 +22,9 @@ def is_contains(string, list_to_search):
             string_to_search = False
     return string_to_search
 
+
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
-print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
 print(calls)
